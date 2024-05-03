08e470b02a97092f2f5b1d4409fe222b06fe4152
1 <168643950+011101110110000101111001@users.noreply.github.com>
2024-05-03 17:05:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,34 +18,4 @@ document.addEventListener('DOMContentLoaded', function() {
         img.style.left = -xMove + 'px';
         img.style.top = -yMove + 'px';
     });
-    const overlayImage = document.querySelector('.overlay img');
-
-    // Перевірка, чи правильно визначено зображення
-    console.log('Overlay Image:', overlayImage);
-
-    // Обробка натискання на зображення
-    overlayImage.addEventListener('click', function() {
-        const popupImage = document.createElement('img');
-        popupImage.src = 'src/you.jpg';  // Вкажіть шлях до нового зображення
-        popupImage.style.position = 'absolute';
-        popupImage.style.top = '50%';
-        popupImage.style.left = '50%';
-        popupImage.style.transform = 'translate(-50%, -50%)';
-        popupImage.style.zIndex = '100'; // Збільшено z-index для кращої видимості
-        document.body.appendChild(popupImage);
-
-        console.log('Попап-зображення має бути зараз видиме');
-
-        // Видалення попап-зображення через 5 секунд
-        setTimeout(function() {
-            document.body.removeChild(popupImage);
-            console.log('Попап-зображення видалено');
-        }, 5000);
-    });
 });
-
-function unmuteAudio() {
-    var audio = document.getElementById("background-music");
-    audio.muted = false;
-    audio.play();
-}
